7878f2a4f367db18fba4bfcd2c798ab3429d99b6
Franciscoxfjanes <franciscoxfjanes@gmail.com>
2023-06-13 15:48:13 +0100

    Commit Alex

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,16 @@
-var anchor = document.querySelector("#navbar").querySelectorAll("a");
+var anchor = document.querySelector("#mainMenu").querySelectorAll("a");
  for( var i = 0; i < anchor.length; i++){
     if(window.location.href == anchor[i].href) {
         anchor[i].classList.add("active");
     }
 
-    if(window.location.href.indexOf("about") > -1) {
+    /* if(window.location.href.indexOf("about") > -1) {
         document.body.classList.add("about");
     } else if (window.location.href.indexOf("contacts") > -1) {
         document.body.classList.add("contacts");
     } else {
         document.body.classList.add("home");
-    }
+    } */
     }
 
     
